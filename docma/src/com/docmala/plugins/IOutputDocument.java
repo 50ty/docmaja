c6d137df52b2c515a4618e297ae7800ece93a23c
Rommel, Stefan <50ty-one@live.de>
2018-04-18 21:51:41 +0200

    Add latex output HINT: install texlive-most under arch

--- docma/src/com/docmala/plugins/IOutputDocument.java
+++ docma/src/com/docmala/plugins/IOutputDocument.java
@@ -3,7 +3,8 @@ package com.docmala.plugins;
 import com.lowagie.text.DocumentException;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public interface IOutputDocument{
-    void write(String fileName) throws FileNotFoundException, DocumentException;
+    void write(String fileName) throws IOException, DocumentException, InterruptedException;
 }
