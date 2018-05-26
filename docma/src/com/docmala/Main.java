70f8d9e341b2cec7679bc4744c7b6a45bb8fe707
Rommel, Stefan <50ty-one@live.de>
2018-05-26 21:32:53 +0200

    Add latex output

--- docma/src/com/docmala/Main.java
+++ docma/src/com/docmala/Main.java
@@ -3,9 +3,10 @@ package com.docmala;
 import com.docmala.parser.ISourceProvider;
 import com.docmala.parser.LocalFileSourceProvider;
 import com.docmala.parser.Parser;
+import com.docmala.plugins.IOutput;
 import com.docmala.plugins.ouput.Html;
+import com.docmala.plugins.ouput.Latex;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 
 public class Main {
@@ -15,18 +16,23 @@ public class Main {
         Parser p = new Parser();
 
         try {
-            ISourceProvider sourceProvider = new LocalFileSourceProvider(Paths.get("/home/michael/projects/docma/docmaja/testdata/"));///home/michael/tmp/"));//testdata/"));
+            String baseDir = "/home/stefan/projects/github/docmaja/testdata/";
+
+            ISourceProvider sourceProvider = new LocalFileSourceProvider(Paths.get(baseDir));///home/michael/tmp/"));//testdata/"));
             //p.parse(sourceProvider, "/home/michael/tmp/DocmaPreviewPlugin.h:part1");
             p.parse(sourceProvider, "test.docma");
             Html htmlOutput = new Html();
             Html.HtmlDocument doc = htmlOutput.generate(p.document());
-            doc.write("/home/michael/tmp/out.html");
+            doc.write(baseDir + "test.html");
+
+            IOutput latexOutput = new Latex(baseDir + "test.docma");
+            latexOutput.generate(p.document()).write(baseDir + "test");
 
             for (Error error : p.errors()) {
                 System.out.printf("%s:(%d,%d):%s%n", error.position().fileName(), error.position().line(), error.position().column(), error.message());
             }
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
